a8685cd3ba1d564cf57a3acb5a4185f99d863524
mohamadjamous <mohamad.e.jamous@gmail.com>
2024-11-17 22:14:23 +0200

    Updated Dependencies

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -23,9 +23,25 @@ kotlin {
         }
     }
 
+    //dependencies versions
+    val coroutinesVersion = "1.6.4"
+    val koinVersion = "3.3.2"
+    val ktorVersion = "2.2.1"
+    val datastoreVersion = "1.1.1"
+
     sourceSets {
         commonMain.dependencies {
             //put your multiplatform dependencies here
+
+            implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutinesVersion")
+            implementation("io.ktor:ktor-client-core:$ktorVersion")
+            implementation("io.ktor:ktor-client-content-negotiation:$ktorVersion")
+            api("io.ktor:ktor-serialization-kotlinx-json:$ktorVersion")
+
+            api("io.insert-koin:koin-core:$koinVersion")
+
+            implementation("androidx.datastore:datastore-preferences-core:$datastoreVersion")
+            implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.6.0-RC.2")
         }
         commonTest.dependencies {
             implementation(libs.kotlin.test)
@@ -46,4 +62,5 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
 }
