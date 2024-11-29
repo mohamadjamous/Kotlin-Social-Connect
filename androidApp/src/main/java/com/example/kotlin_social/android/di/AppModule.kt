0a9a15fe56657f90bf5554ac33b69e36dd7b7b0a
mohamadjamous <mohamad.e.jamous@gmail.com>
2024-11-29 22:27:21 +0200

    HomeScreen UI

--- androidApp/src/main/java/com/example/kotlin_social/android/di/AppModule.kt
+++ androidApp/src/main/java/com/example/kotlin_social/android/di/AppModule.kt
@@ -7,6 +7,7 @@ import com.example.kotlin_social.android.auth.SignUpViewModel
 import com.example.kotlin_social.android.auth.login.LoginViewModel
 import com.example.kotlin_social.android.common.datastore.UserSettings
 import com.example.kotlin_social.android.common.datastore.UserSettingsSerializer
+import com.example.kotlin_social.android.home.HomeScreenViewModel
 import org.koin.android.ext.koin.androidContext
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
@@ -15,6 +16,7 @@ val appModule = module {
     viewModel { LoginViewModel(get()) }
     viewModel { SignUpViewModel(get()) }
     viewModel { MainActivityViewModel(get()) }
+    viewModel { HomeScreenViewModel() }
 
 
     single {
