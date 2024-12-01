8a22681b109f9696d3a72ff50d2870c2d0e4b235
mohamadjamous <mohamad.e.jamous@gmail.com>
2024-12-01 18:44:39 +0200

    Post Structure

--- androidApp/src/main/java/com/example/kotlin_social/android/post/PostDetailScreen.kt
+++ androidApp/src/main/java/com/example/kotlin_social/android/post/PostDetailScreen.kt
@@ -1,9 +1,12 @@
 package com.example.kotlin_social.android.post
 
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.OutlinedButton
 import androidx.compose.material.Text
@@ -12,13 +15,38 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import com.example.kotlin_social.android.R
+import com.example.kotlin_social.android.common.components.PostListItem
+import com.example.kotlin_social.android.common.fake_data.Comment
 import com.example.kotlin_social.android.common.theming.LargeSpacing
 
 
 @Composable
 fun PostDetailScreen(
-    modifier: Modifier
+    modifier: Modifier = Modifier,
+    postUiState: PostUiState,
+    commentsUiState: CommentsUiState,
+    onCommentMoreIconClick: (Comment) -> Unit,
+    onProfileClick: (Long) -> Unit,
+    onAddCommentClick: () -> Unit
 ){
+
+    if (postUiState.isLoading && commentsUiState.isLoading){
+        Box(modifier = modifier.fillMaxWidth(), contentAlignment = Alignment.Center){
+            CircularProgressIndicator()
+        }
+    }else if (postUiState.post != null){
+        LazyColumn(
+            modifier = modifier.fillMaxWidth()) {
+
+            item (key = "post_item") {
+                PostListItem(
+                    post = postUiState.post,
+                    onProfileClick = onProfileClick,
+                    onLikeClick = {},
+                    onCommentClick = {})
+            }
+        }
+    }
 
 }
 
